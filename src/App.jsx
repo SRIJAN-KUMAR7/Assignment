4997bc39892337022bbbb9d2ea7f676c3f7bb874
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-06-21 14:25:02 +0530

    deploying

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import React from 'react'
-import AddItem from './components/AddItems'
+import AddItem from './components/Additem'
 import ViewItems from './components/ViewItems'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
